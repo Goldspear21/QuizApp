d0b6b056e480a0b1946fa65401a455e60ce074c0
Bhargav <bhargavsuresh@outlook.com>
2025-05-24 20:41:39 +0400

    commit

--- quiz_app_package/app/run_app.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set FX_PATH=%~dp0..\javafx-sdk-23.0.2\lib
-java --module-path "%FX_PATH%" --add-modules javafx.controls,javafx.fxml,javafx.media -jar "%~dp0target\quiz_app-1.0-SNAPSHOT.jar"
-pause
